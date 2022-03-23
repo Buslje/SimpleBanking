37c7546975f88a9be0955da07f818e9bc1cee656
Luka Bušljeta <busljetaluka@gmail.com>
2022-03-23 13:36:11 +0100

    SimpleBanking drugi commit

--- Simple Banking System/task/src/banking/CreditCard.java
+++ Simple Banking System/task/src/banking/CreditCard.java
@@ -1,39 +1,45 @@
 package banking;
 
 
+import static banking.Main.random;
+
 public class CreditCard {
-    String bin = "400000";
-    String accountIndentifier;
-    String checkSum = "5";
 
-    public CreditCard(String bin, String accountIndentifier, String checkSum) {
-        this.bin = bin;
-        this.accountIndentifier = accountIndentifier;
-        this.checkSum = checkSum;
+    private static String cardNum;
+    private static String pin;
+
+
+
+    public static String createAccountNumber(String cardNum) {
+        String bankNumber = "400000";
+        int lowerAccountNumber = 100000000;
+        int upperAccountNumber = 999999999;
+        int accountNumber = random.nextInt(upperAccountNumber - lowerAccountNumber + 1) + lowerAccountNumber;
+        String accountDigits = bankNumber + accountNumber;
+
+        int sum = 0;
+        boolean alternate = false;
+
+        for (int i = accountDigits.length() - 1; i >= 0; --i) {
+            int digit = Character.getNumericValue(accountDigits.charAt(i));
+            digit = (alternate = !alternate) ? (digit * 2) : digit;
+            digit = (digit > 9) ? (digit - 9) : digit;
+            sum += digit;
+        }
+
+        int lastDigit = (sum * 9) % 10;
+
+        cardNum = accountDigits + String.valueOf(lastDigit);
+        return cardNum;
     }
 
-    public String getBin() {
-        return bin;
+    public static String createPin(String pin) {
+        int lower = 1000;
+        int upper = 9999;
+        pin = String.valueOf(random.nextInt(upper - lower + 1) + lower);
+        return pin;
     }
 
-    public void setBin(String bin) {
-        this.bin = bin;
-    }
-
-    public String getAccountIndentifier() {
-        return accountIndentifier;
-    }
-
-    public void setAccountIndentifier(String accountIndentifier) {
-        this.accountIndentifier = accountIndentifier;
-    }
-
-    public String getCheckSum() {
-        return checkSum;
-    }
-
-    public void setCheckSum(String checkSum) {
-        this.checkSum = checkSum;
-    }
 }
+
 
--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -6,24 +6,22 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
+import static banking.CreditCard.*;
+
 public class Main {
+
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
-    static String cardNum ="";
-    static String accountNumber ="";
-    static int nCard = 0;
-    static int pinNumber =0;
-    static String pin ="";
-    static String bin ="";
-    static String accId ="";
-    static String checkSum ="";
-    static boolean menuShow = false;
     static List<String> accountInfo= new ArrayList<String>();
+
+    static boolean isTrue = false;
+
     public static void main(String[] args) {
-        CreditCard card = new CreditCard("400000","100000000 + rnd.nextInt(900000000)","5");
+
+        boolean isTrue = false;
         int choice = 0;
         do {
-            if (menuShow == false) {
+            if (isTrue == false) {
                 menu1();
             } else {
                 break;
@@ -31,71 +29,55 @@ public class Main {
             choice = scanner.nextInt();
             switch (choice) {
                 case 1:
-                    CreateAnAccount(card);
+                    createAnAccount();
                     break;
                 case 2:
-                    LogIntoAccount(card);
+                    logIntoAccount();
             }
         } while (choice != 0);
     }
 
-        public static void CreateAnAccount(CreditCard card) {
-
-            System.out.println("Your card number has been created");
-            System.out.println("Your card number:");
-            cardNum = CreateAccountNumber(bin, accId, nCard);
-            System.out.println(cardNum);
-            System.out.println("Your card PIN:");
-            pin = CreatePin(pinNumber, pin);
-            System.out.println(pin);
-            setCardsAndPins(cardNum,pin);
-
+    public static void createAnAccount() {
+        String cardNum ="";
+        String pin ="";
+        System.out.println("Your card number has been created");
+        System.out.println("Your card number:");
+        cardNum = createAccountNumber(cardNum);
+        System.out.println(cardNum);
+        System.out.println("Your card PIN:");
+        pin = createPin(pin);
+        System.out.println(pin);
+        setCardsAndPins(cardNum, pin);
     }
-        public static void LogIntoAccount(CreditCard card) {
-            boolean menu = false;
-            System.out.println("Enter your card number:");
-            String cn = scanner.next();
-            System.out.println("Enter your PIN:");
-            String p = scanner.next();
+    public static void logIntoAccount() {
 
+        boolean menu = false;
+        System.out.println("Enter your card number:");
+        String cn = scanner.next();
+        System.out.println("Enter your PIN:");
+        String p = scanner.next();
 
-
-            for (String num: accountInfo){
-                if (num.equals(cn) && accountInfo.get(accountInfo.indexOf(num)+1).equals(p)) {
-                    System.out.println("You have successfully logged in!\n");
-                    menu = true;
-                }
+        for (String num: accountInfo){
+            if (num.equals(cn) && accountInfo.get(accountInfo.indexOf(num)+1).equals(p)) {
+                System.out.println("You have successfully logged in!\n");
+                menu = true;
             }
-            if (menu == true){
-                menu2();
-            } else {
-                System.out.println("Wrong card number or PIN!");
-            }
+        }
+        if (menu == true){
+            menu2();
+        } else {
+            System.out.println("Wrong card number or PIN!");
+        }
 
     }
-    public static String CreateAccountNumber(String bin,String accId,int nCard) {
-        bin = "400000";
-        nCard = 100000000 + random.nextInt(900000000);
-        accId = Integer.toString(nCard);
-        cardNum = bin + nCard + "5";
-        return cardNum;
-    }
-    public static String CreatePin(int pinNumber, String pin) {
-    pinNumber = 1000 + random.nextInt(9000);
-    pin = Integer.toString(pinNumber);
-    return pin;
-    }
 
-    public static void setCardsAndPins(String cardNum, String pin) {
-        accountInfo.add(cardNum);
-        accountInfo.add(pin);
-    }
     public static void menu1() {
+
         System.out.println("1. Create an account\n"+
                 "2. Log into account\n" +
                 "0. Exit");
     }
-    public static void menu2(){
+    public static void menu2() {
         int option = 3;
         System.out.println("1. Balance\n" +
                 "2. Log out\n" +
@@ -114,10 +96,15 @@ public class Main {
             }
             else if (option == 0){
                 System.out.println("Bye!");
-                menuShow = true;
+                isTrue = true;
                 break;
             }
         }while(option!=0);
     }
 
+    public static void setCardsAndPins(String cardNum, String pin) {
+        accountInfo.add(cardNum);
+        accountInfo.add(pin);
+    }
 }
+
